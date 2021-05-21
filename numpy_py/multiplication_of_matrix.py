7d0fbfa8566f871cf8d3431f2726db94a7013700
abhimanoj <abhimanojyadav@gmail.com>
2021-05-22 00:42:37 +0530

    numpy bt

--- numpy_py/multiplication_of_matrix.py
+++ numpy_py/multiplication_of_matrix.py
@@ -12,11 +12,22 @@ class Multiplication:
 
 
       def operation(self):
+
+            z = np.zeros(10)
+            z[4] = 1
+            np_rng = 10 + np.arange(99)
             self.result = f"""
-                        Reuslt is: { np.dot(self.input_1, self.input_2)}
+                        Version:{np.__version__} <br>
+                        np zeros: {np.zeros(10)} <br>
+                        np zeros except one value: {z} <br>
+                        values ranging from 10 to 99: <br>{np_rng} <br> <br>
+                        Reuslt is multiplication: { np.dot(self.input_1, self.input_2)}
                         """
             return self.result
 
       def __str__(self):
             
             return str(self.result)
+
+
+print(Multiplication().operation())
